3e053a1031b3b5be9ed4cb7b71df0dc97bc0c37f
MSDesignProjects <mishasofil@gmail.com>
2025-12-12 12:42:38 +0000

    Update forum.js

--- forum.js
+++ forum.js
@@ -1,17 +1,37 @@
 // forum.js - Earthy color sticky note forum
 
-// Initialize Firebase (replace with your config)
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Firebase imports
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  onSnapshot,
+  query,
+  orderBy
+} from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
-  };
-  
-  firebase.initializeApp(firebaseConfig);
-  const db = firebase.firestore();
+  apiKey: "AIzaSyBKQOL2ek5inD7ZKAbkL5KYX6IxMQDhxpM",
+  authDomain: "hiddenneighbourhoodforum.firebaseapp.com",
+  projectId: "hiddenneighbourhoodforum",
+  storageBucket: "hiddenneighbourhoodforum.firebasestorage.app",
+  messagingSenderId: "822389042504",
+  appId: "1:822389042504:web:b5f2a900dc588336e2577d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
   
   // Earthy colour palette
   const earthyColors = [
